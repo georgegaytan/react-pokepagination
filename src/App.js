a4da2fc53ccae0792d32f109ffb61fddceb123f3
ggaytan00 <coolkidgg@yahoo.com>
2019-09-23 23:36:13 -0400

    test app complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import PokeList from "./PokeList";
+import Pagination from "./Pagination";
+import axios from "axios";
 
 function App() {
-  return (
-    null
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [previousPageUrl, setPreviousPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken(c => (cancel = c))
+      })
+      .then(result => {
+        setLoading(false);
+        setNextPageUrl(result.data.next);
+        setPreviousPageUrl(result.data.previous);
+        setPokemon(result.data.results.map(poke => poke.name));
+      });
+
+    return () => cancel();
+  }, [currentPageUrl]);
+
+  function toNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+  function toPreviousPage() {
+    setCurrentPageUrl(previousPageUrl);
+  }
+
+  return loading ? (
+    "Loading..."
+  ) : (
+    <React.Fragment>
+      <PokeList pokemon={pokemon} />
+      <Pagination toNextPage={nextPageUrl ? toNextPage : null} toPreviousPage={previousPageUrl ? toPreviousPage : null} />
+    </React.Fragment>
   );
 }
 
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Pagination() {
+export default function Pagination({ toNextPage, toPreviousPage }) {
     return (
         <div>
-            
+            { toPreviousPage && <button variant="primary" onClick={toPreviousPage}>Previous</button> }
+            { toNextPage && <button variant="primary" onClick={toNextPage}>Next</button> }
         </div>
     )
 }
